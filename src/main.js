be459890937910674d33c50106496dd1540b5663
lehn <laurence.for5@gmail.com>
2024-07-31 15:47:15 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링을 적용
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
