4331cbb0413363f8d8c1cd4269d45437790785e6
lehn <laurence.for5@gmail.com>
2024-12-19 11:11:56 +0900

    Observer-part1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// Header에 페이지 아래로 스크롤시 다크 스타일링을 적용
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
+
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
@@ -14,29 +15,65 @@ document.addEventListener('scroll', () => {
 // Home 섹션을 아래로 스크롤시 투명하게 처리함
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
+
 document.addEventListener('scroll', () => {
-  console.log(1 - window.scrollY / homeHeight);
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+// 스크롤 시 홈 섹션 절반부터 Arrow up 보임
 const arrowUp = document.querySelector('.arrow-up');
+
 document.addEventListener('scroll', () => {
-  if (window.scrollY > homeHeight / 2) {
+  if (homeHeight / 2 < window.scrollY) {
     arrowUp.style.opacity = 1;
   } else {
     arrowUp.style.opacity = 0;
   }
 });
 
-// Navbar 토글버튼 클릭 처리
-const navbarMenu = document.querySelector('.header__menu');
+// Navar 토글버튼 클릭 처리
 const navbarToggle = document.querySelector('.header__toggle');
+const navbarMenu = document.querySelector('.header__menu');
+
 navbarToggle.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+// Navar 메뉴 클릭시 메뉴를 자동으로 닫아줌
 navbarMenu.addEventListener('click', () => {
   navbarMenu.classList.remove('open');
 });
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
